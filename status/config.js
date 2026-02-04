67314116839f1ff770cb1629bb6c36e8325d2c91
HelloWorld <76789158+zixibar@users.noreply.github.com>
2026-02-04 22:02:35 +0800

    Add files via upload

---
+++ status/config.js
@@ -0,0 +1,34 @@
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: 'Svipcun状态监控',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
+  ApiKeys: [
+    'ur2523928-aa791a010b5f4b5df4486aff',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: false,
+
+  // 日志天数
+  // 默认免费版保留 30 天，付费可取 90 天；不要设置太久以免接口失败
+  CountDays: 30,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Svip村',
+      url: 'https://svipcun.com/'
+    },
+    {
+      text: '发布页',
+      url: 'https://zixibar.github.io/'
+    },
+  ]
+};
